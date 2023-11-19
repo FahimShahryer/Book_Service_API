f055143923748ef0ea3e4516d4da2ced637686a7
FahimShahryer <56716260+FahimShahryer@users.noreply.github.com>
2023-11-19 23:37:49 +0600

    Update session.py

--- app/database/session.py
+++ app/database/session.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 
 from app.database.base import Base
 
-DATABASE_URL = "postgresql://fahim:50569@localhost/Book_Server"
+DATABASE_URL = "postgresql://<user>:<password>@localhost/<Database_Name>"
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
